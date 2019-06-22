b3b30075bbc072d9250d75c74a1cb3d3fba28c2b
Shawn Moore <sm5@me.com>
2019-06-21 23:26:33 -0700

    Initial Commit

---
+++ CollectionView/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CollectionView
+//
+//  Created by Shawn Moore on 6/21/19.
+//  Copyright © 2019 Shawn Moore. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
